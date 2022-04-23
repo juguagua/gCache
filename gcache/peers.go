27180ad2bc710010f3379d096705ec139570ee11
juguagua <juguaguaji@gmail.com>
2022-04-23 23:54:52 +0800

    main.go: add startCacheServe

--- gcache/peers.go
+++ gcache/peers.go
@@ -3,7 +3,7 @@ package gcache
 // PeerPicker 是用来定位的接口
 type PeerPicker interface {
 	// PickPeers 用于根据传入的key选择合适的PeerGetter节点
-	PickPeers(key string) (peer PeerGetter, ok bool)
+	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 // PeerGetter 相当于一个客户端，每个客户端对象需要实现这个接口
