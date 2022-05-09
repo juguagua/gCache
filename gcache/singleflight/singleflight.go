4612b94cd102d5540ffdaa6e0bfa70dd4645d246
juguagua <juguaguaji@gmail.com>
2022-05-09 23:07:19 +0800

    singleflight

--- gcache/singleflight/singleflight.go
+++ gcache/singleflight/singleflight.go
@@ -5,9 +5,35 @@ import "sync"
 type call struct { // 表示正在进行中，或已经结束的请求
 	wg  sync.WaitGroup // 避免重入
 	val interface{}
+	err error
 }
 
 type Group struct { // 管理不同key的请求（call）
 	mu sync.Mutex
 	m  map[string]*call
+}
+
+func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	g.mu.Lock()
+	if g.m == nil {
+		g.m = make(map[string]*call)
+	}
+	if c, ok := g.m[key]; ok {
+		g.mu.Unlock()
+		c.wg.Wait()
+		return c.val, c.err
+	}
+	c := new(call)
+	c.wg.Add(1)
+	g.m[key] = c
+	g.mu.Unlock()
+
+	c.val, c.err = fn()
+	c.wg.Done()
+
+	g.mu.Lock()
+	delete(g.m, key)
+	g.mu.Unlock()
+
+	return c.val, c.err
 }
